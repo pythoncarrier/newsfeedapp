1e6cf3364723422f09334e9b36f0a44d6673b7f3
Artem Yakovenko <artemyakovenko16@gmail.com>
2018-09-13 08:38:05 +0300

    Implement get weather request

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-import feedparser
+import feedparser, json
 from flask import Flask, render_template, request
+from urllib.request import urlopen
+from urllib.parse import quote
 
 app = Flask("__name__")
 
@@ -7,6 +9,8 @@ RSS_FEEDS = {
     'proglib': "https://proglib.io/feed/",
     'hltv': "https://www.hltv.org/rss/news"
 }
+
+OWM_API_KEY = 'cc1b5c72e6f1a10bfbf3f0338255f73f'
 
 
 @app.route('/')
@@ -18,6 +22,21 @@ def index():
         feed_type = feed_type.lower()
     feed = feedparser.parse(RSS_FEEDS[feed_type])
     return render_template('index.html', articles=feed['entries'])
+
+
+def get_weather(query):
+    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
+    url = api_url.format(quote(query), OWM_API_KEY)
+    response = urlopen(url).read()
+    data = json.loads(response)
+    weather = None
+    if data["weather"]:
+        weather = {
+            "description": data["weather"][0]["description"],
+            "temperature": data["main"]["temp"],
+            "city": data["name"]
+        }
+    return weather
 
 
 if __name__ == "__main__":
