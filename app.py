c6e08c8692af985f7962b31b359b046dda91fde0
Artem Yakovenko <artemyakovenko16@gmail.com>
2018-09-13 08:05:33 +0300

    Implement search for list of articles

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import feedparser
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("__name__")
 
-PROGLIB_FEED = "https://proglib.io/feed/"
+RSS_FEEDS = {
+    'proglib': "https://proglib.io/feed/",
+    'hltv': "https://www.hltv.org/rss/news"
+}
 
 
 @app.route('/')
 def index():
-    feed = feedparser.parse(PROGLIB_FEED)
-    article = feed['entries'][0]
-    return render_template('index.html', title=article.get('title'),
-                           summary=article.get('summary', published=article.get('published')))
+    feed_type = request.args.get("site")
+    if not feed_type or feed_type.lower() not in RSS_FEEDS:
+        feed_type = 'proglib'
+    else:
+        feed_type = feed_type.lower()
+    feed = feedparser.parse(RSS_FEEDS[feed_type])
+    return render_template('index.html', articles=feed['entries'])
 
 
 if __name__ == "__main__":
