5e69b38b858a03eba91d009ac90f4b9a553ba3fa
Artem Yakovenko <artemyakovenko16@gmail.com>
2018-09-14 08:29:01 +0300

    Implement weather search

--- app.py
+++ app.py
@@ -7,26 +7,41 @@ app = Flask("__name__")
 
 RSS_FEEDS = {
     'proglib': "https://proglib.io/feed/",
-    'hltv': "https://www.hltv.org/rss/news"
+    'hltv': "https://www.hltv.org/rss/news",
+}
+
+DEFAULTS = {
+    'feed_type': 'proglib',
+    'weather_city': 'Kiev',
 }
 
 OWM_API_KEY = 'cc1b5c72e6f1a10bfbf3f0338255f73f'
+WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
 
 
 @app.route('/')
 def index():
     feed_type = request.args.get("site")
-    if not feed_type or feed_type.lower() not in RSS_FEEDS:
-        feed_type = 'proglib'
+    weather_city = request.args.get("city")
+
+    articles = get_news(feed_type)
+    weather = get_weather(weather_city)
+    return render_template('index.html', articles=articles['entries'], weather=weather)
+
+
+def get_news(query):
+    if not query or query.lower() not in RSS_FEEDS:
+        feed_type = DEFAULTS['feed_type']
     else:
-        feed_type = feed_type.lower()
-    feed = feedparser.parse(RSS_FEEDS[feed_type])
-    return render_template('index.html', articles=feed['entries'])
+        feed_type = query.lower()
+    return feedparser.parse(RSS_FEEDS[feed_type])
 
 
 def get_weather(query):
-    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
-    url = api_url.format(quote(query), OWM_API_KEY)
+    if not query:
+        query = DEFAULTS['weather_city']
+
+    url = WEATHER_URL.format(quote(query), OWM_API_KEY)
     response = urlopen(url).read()
     data = json.loads(response)
     weather = None
@@ -34,7 +49,8 @@ def get_weather(query):
         weather = {
             "description": data["weather"][0]["description"],
             "temperature": data["main"]["temp"],
-            "city": data["name"]
+            "city": data["name"],
+            "country": data["sys"]["country"]
         }
     return weather
 
